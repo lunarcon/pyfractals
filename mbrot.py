bfd973228252d1437d8226de83717262decf5274
Aditya Vikram <adityavikram2016@gmail.com>
2020-01-11 11:44:50 +0530

    updated mrbrot

--- mbrot.py
+++ mbrot.py
@@ -2,7 +2,7 @@ from PIL import Image
 from numpy import complex, array 
 import colorsys
 import math
-WIDTH = 4096
+w = 4096
 def rgb_conv(i): 
     color = 255 * array(colorsys.hsv_to_rgb(1.0, i / 255.0, i / 255.0)) 
     return tuple(color.astype(int)) 
@@ -14,11 +14,11 @@ def mbrot(x, y):
             return rgb_conv(i) 
         c = c * c + c0 
     return (0, 0, 0) 
-img = Image.new('RGB', (WIDTH, int(WIDTH / 2))) 
+img = Image.new('RGB', (w, int(w / 2))) 
 pixels = img.load()   
 for x in range(img.size[0]): 
-    print("","%.2f %%" % (x / WIDTH * 100.0),"█"*math.ceil(x / WIDTH * 25.0) + "▒"*(25-math.ceil(x / WIDTH * 25.0)),end="\r")  
+    print("","%.2f %%" % (x / w * 100.0),"█"*math.ceil(x / w * 25.0) + "▒"*(25-math.ceil(x / w * 25.0)),end="\r")  
     for y in range(img.size[1]): 
-        pixels[x, y] = mbrot((x - (0.75 * WIDTH)) / (WIDTH / 4), 
-                                      (y - (WIDTH / 4)) / (WIDTH / 4)) 
+        pixels[x, y] = mbrot((x - (0.75 * w)) / (w / 4), 
+                                      (y - (w / 4)) / (w / 4)) 
 img.save("fractal.bmp")
